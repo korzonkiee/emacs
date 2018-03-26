0fa6542a110f90fe337fe7725c6a4e3a26320406
Korzonkie <rootie0011@gmail.com>
2018-03-26 18:01:14 +0200

    Configure Rust

--- init.el
+++ init.el
@@ -3,40 +3,33 @@
 ;; INSTALL PACKAGES
 ;; --------------------------------------
 
-(require 'package)
 
-(add-to-list 'package-archives
-       '("melpa" . "http://melpa.org/packages/") t)
-
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
 (package-initialize)
-(when (not package-archive-contents)
-  (package-refresh-contents))
 
-(defvar myPackages
-  '(better-defaults
-    elpy
-    material-theme))
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
-(mapc #'(lambda (package)
-    (unless (package-installed-p package)
-      (package-install package)))
-      myPackages)
+(require 'init-elpa)
+(require 'init-ui)
+(require 'init-navigation)
+(require 'init-miscellaneous)
+(require 'init-editing)
+(require 'init-company-mode)
 
-;; BASIC CUSTOMIZATION
-;; --------------------------------------
+(require 'init-rust)
 
-(setq inhibit-startup-message t) ;; hide the startup message
-(load-theme 'material t) ;; load material theme
-(global-linum-mode t) ;; enable line numbers globally
-(elpy-enable)
-
-;; init.el ends here
+(provide 'init)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (material-theme better-defaults))))
+ '(package-selected-packages
+   (quote
+    (## cargo flycheck-rust racer flycheck rainbow-delimiters projectile smex pkg-info material-theme ido-ubiquitous golden-ratio elpy better-defaults atom-one-dark-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
